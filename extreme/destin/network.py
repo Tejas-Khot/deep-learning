751c39d8a0e578904a5ad39ebe9b4737c89e3821
Tejas Khot <tjskhot@gmail.com>
2015-06-20 13:36:14 +0530

    added extreme testing code

---
+++ extreme/destin/network.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+__author__ = 'teddy'
+"""
+@note: An implementation of DeSTIN Network architecture 
+"""
+import scipy.io as io
+from load_data import *
+from layer import *
+import theano
+from theano.tensor.signal import downsample
+import theano.tensor as T
+import numpy as np
+import math
+# io.savemat(file_name,Dict,True)
+# TODO: get ridoff the sequential requirements like first feed the layer
+# an input the you can initialize it
+
+
+class Network():
+    """
+    An implementation of DeSTIN 
+
+    This class creates the network. 
+    """
+
+    def __init__(self, 
+                 num_layers, 
+                 alg_choice, 
+                 alg_params, 
+                 num_nodes_per_layer, 
+                 cifar_stat=[], 
+                 patch_mode='Adjacent', 
+                 image_type='Color'):
+
+        """
+        Initialize DeSTIN
+
+        @param num_layers: number of layers 
+        @param alg_choice: choice of algorithm - one from ["Clustering", "Auto-Encoder", "LogRegresion"]
+        @param alg_params: Dictionary of various parameters needed for learning algorithm 
+        @param num_nodes_per_layer: number of nodes present in each layer of DeSTIN
+        @param cifar_stat: parameters needed for the CIFAR dataset
+        @param patch_mode: patch selection mode for images 
+        @param image_type: type of images to be used
+        """
+
+        self.network_belief = {}
+        self.lowest_layer = 1
+        # this is going to store beliefs for every image DeSTIN sees
+        self.network_belief['belief'] = np.array([])
+        self.save_belief_option = 'True'
+        self.belief_file_name = 'beliefs.mat'
+        self.number_of_layers = num_layers
+        self.algorithm_choice = alg_choice
+        self.algorithm_params = alg_params
+        self.number_of_nodesPerLayer = num_nodes_per_layer
+        self.patch_mode = patch_mode
+        self.image_type = image_type
+        self.layers = [
+            [Layer(j, num_nodes_per_layer[j], cifar_stat, self.patch_mode, self.image_type) for j in range(num_layers)]]
+
+    def setmode(self, mode):
+        """
+        Sets the network training mode
+
+        @param mode: True indicates that the network is training
+        """
+        self.operating_mode = mode
+        for I in range(self.number_of_layers):
+            self.layers[0][I].mode = mode
+
+    def init_network(self):
+        """
+        Initialize all layers of the network
+        """
+        for L in range(self.number_of_layers):
+            self.initLayer(L)
+
+    def set_lowest_layer(self, lowest_layer):
+        """
+        Sets the indicated layer to be the lowest one
+
+        @param lowest_layer: value of lowest network layer i.e. starting layer
+        """
+        self.lowest_layer = lowest_layer
+
+    def initLayer(self, layer_num):
+        """
+        Initialize all layers with the learning parameters
+
+        @param layer_num: layer number 
+        """
+        self.layers[0][layer_num].init_layer_learning_params(self.algorithm_choice, 
+                                                            self.algorithm_params)
+
+    def train_layer(self, layer_num):
+        """
+        Train all layers based on the network mode
+
+        @param layer_num: layer number to train 
+        """
+        self.layers[0][layer_num].do_layer_learning(self.operating_mode)
+
+    def update_belief_exporter(self):
+        """
+        Updates the belief states across the network
+        """
+        for i in range(self.lowest_layer, self.number_of_layers):
+            for j in range(len(self.layers[0][i].nodes)):
+                for k in range(len(self.layers[0][i].nodes[0])):
+                    if self.network_belief['belief'] == np.array([]):
+                        self.network_belief['belief'] = np.array(self.layers[0][i].nodes[j][k].belief).ravel()
+                    else:
+                        self.network_belief['belief'] = np.hstack((np.array(self.network_belief['belief']),
+                                                                  np.array(self.layers[0][i].nodes[j][k].belief).ravel()))
+
+    def update_belief_exporter(self, maxpool_shape , ignore_border, mode):
+        input = T.dmatrix('input')
+        for i in range(self.lowest_layer, self.number_of_layers):
+            pool_out = downsample.max_pool_2d(input, maxpool_shape[i] , ignore_border=ignore_border)   
+            f = theano.function([input],pool_out)    #function of pooling belief vector, maxpool_shape[i] = pool size
+            temp_belief = np.array([])
+            for j in range(len(self.layers[0][i].nodes)):
+                for k in range(len(self.layers[0][i].nodes[0])):
+                    if temp_belief == np.array([]):
+                        temp_belief = np.array(self.layers[0][i].nodes[j][k].belief).ravel()
+                    else:
+                        temp_belief = np.hstack((np.array(temp_belief),
+                                                np.array(self.layers[0][i].nodes[j][k].belief).ravel()))           
+                        
+            no_of_centroids = self.algorithm_params['num_cents_per_layer'][i]
+            no_of_nodes = self.number_of_nodesPerLayer[i][0]**2
+            temp_belief = temp_belief.reshape(no_of_nodes,no_of_centroids)
+
+            if no_of_nodes != 4 and no_of_nodes != 1:
+                temp_belief_a = temp_belief[0:no_of_nodes/2,:]                  # split the temp_belief into two parts 
+                temp_belief_b = temp_belief[no_of_nodes/2:no_of_nodes,:]        
+                matrix_a = np.zeros((no_of_nodes/4, no_of_centroids))           
+                matrix_b = np.zeros((no_of_nodes/4, no_of_centroids))          
+                temp_belief = np.array([])
+                a=0 
+                b=0
+                a2=0 
+                b2=0
+                a1=0
+                b1=0
+                for n in range(no_of_nodes/2):
+                    if no_of_nodes == 16:                              #rearrange the node order in the second layer
+                        if ( n / 2 )%2 == 0 :
+                            matrix_a[a,:] = temp_belief_a[n,:]
+                            a=a+1
+                        else: 
+                            matrix_b[b,:] = temp_belief_a[n,:]
+                            b=b+1
+
+                    elif no_of_nodes == 64:                                #rearrange the node order in the first layer
+                        if ( n / 4 )%2 == 0 :  # (n/4)%2
+                            if ((n / 2) % 2) == 0 : 
+                                matrix_a[a1,:] = temp_belief_a[n,:]
+                            else:
+                                matrix_a[a1+4,:] = temp_belief_a[n,:]  
+                                a1=a1+1                      
+                        else : 
+                            if (n / 2) % 2 == 0 : 
+                                matrix_b[b1,:] = temp_belief_a[n,:]
+                            else:
+                                matrix_b[b1+4,:] = temp_belief_b[n,:]
+                                b1=b1+1
+
+                temp_belief = np.hstack((np.array(matrix_a).ravel(),np.array(matrix_b).ravel())) 
+                a=0 
+                b=0
+
+                for n in range(no_of_nodes/2):
+                    if no_of_nodes ==16 :                             #rearrange the node order in the second layer
+                        if ( n / 2 )%2 == 0 :
+                            matrix_a[a,:] = temp_belief_b[n,:]
+                            a=a+1
+                        else: 
+                            matrix_b[b,:] = temp_belief_b[n,:]           
+                            b=b+1
+
+                    elif no_of_nodes == 64:                    #rearrange the node order in the first layer
+                        if ( n / 4 )%2 == 0 :  # (n/4)%2
+                            if ((n / 2) % 2) == 0 : 
+                                matrix_a[a1,:] = temp_belief_b[n,:]
+                            else:
+                                matrix_a[a2+4,:] = temp_belief_b[n,:]  
+                                a2=a2+1                      
+                    else : 
+                        if (n / 2) % 2 == 0 : 
+                            matrix_b[b2,:] = temp_belief_b[n,:]
+                        else:
+                            matrix_b[b2+4,:] = temp_belief_b[n,:]
+                            b2=b2+1
+ 
+                temp_belief = np.hstack((np.array(temp_belief),np.array(matrix_a).ravel())) 
+                temp_belief = np.hstack((np.array(temp_belief),np.array(matrix_b).ravel()))
+                temp_belief = temp_belief.reshape(no_of_nodes,no_of_centroids)  
+
+            #invals = np.array(temp_belief)
+            pool_temp_belief=(f(temp_belief))
+            #print 'pool',pool_temp_belief
+            if self.network_belief['belief'] == np.array([]):
+                self.network_belief['belief'] = np.array(pool_temp_belief).ravel()
+            else:
+                self.network_belief['belief'] = np.hstack((np.array(self.network_belief['belief']),
+                                                          np.array(pool_temp_belief).ravel()))
+            #print self.network_belief['belief'].shape
+
+    def dump_belief(self, num_of_images):
+        """
+        Saving belief states for all images in a file 
+
+        @param num_of_images: number of images 
+        """
+        total_belief_len = len(np.array(self.network_belief).ravel())
+        single_belief_len = total_belief_len / num_of_images
+        print np.array(self.network_belief).ravel()
+        belief = np.array(self.network_belief).reshape(num_of_images, single_belief_len)
+        io.savemat(self.belief_file_name, belief)
+
+    def clean_belief_exporter(self):
+        """
+        Delete all beliefs i.e. clean instantiation
+        """
+        self.network_belief['belief'] = np.array([])
---
+++ extreme/test_conv_destin.py
@@ -0,0 +1,249 @@
+__author__ = 'tejas'
+from destin.network import *
+from destin.load_data import *
+import cPickle as pickle
+from time import time
+from sklearn import svm
+import os
+# *****Define Parameters for the Network and nodes
+
+# Network Params
+num_layers = 4
+patch_mode = 'Adjacent'
+image_type = 'Color'
+network_mode = True
+# For a Node: specify Your Algorithm Choice and Corresponding parameters
+
+# ******************************************************************************************
+#
+#                           Incremental Clustering
+#
+num_nodes_per_layer = [[8, 8], [4, 4], [2, 2], [1, 1]]
+num_cents_per_layer = [1000, 75, 50 ,25]
+pool_size = [(1,1),(1,1),(1,1),(1,1)]         #pooling size: The first number is the number of vector 
+                                              #you want to pool. For example, (64,1) will pool all the 
+                                              #vector in the first layer. (16,1) will divide the first layer 
+                                              #in to 4 quarters and pool each of them. (4,1) will divide the 
+                                              #first layer in to 16th pieces and pool each of them
+print "Uniform DeSTIN with Clustering"
+algorithm_choice = 'Clustering'
+alg_params = {'mr': 0.01, 'vr': 0.01, 'sr': 0.001, 'DIMS': [],
+             'CENTS': [], 'node_id': [],
+             'num_cents_per_layer': num_cents_per_layer}
+# ******************************************************************************************
+'''
+#  ******************************************************************************************
+
+#           Hierarchy Of AutoEncoders
+
+print "Uniform DeSTIN with AutoEncoders"
+num_nodes_per_layer = [[8, 8], [4, 4], [2, 2], [1, 1]]
+num_cents_per_layer = [36, 36, 36, 36]
+algorithm_choice = 'AutoEncoder'
+inp_size = 48
+hid_size = 100
+alg_params = [[inp_size, hid_size], [4 * hid_size, hid_size],
+             [4 * hid_size, hid_size], [4 * hid_size, hid_size]]
+#  ******************************************************************************************
+'''
+
+#Load Data, 10 loads 5 batches in total 50,000
+# 1 to 5 load batch_1 to batch_5training images, 1 to five
+[data, labels] = loadCifar(10)
+del labels
+
+# Declare a Network Object and load Training Data
+cifar_stat = load_cifar(4)
+DESTIN = Network(
+    num_layers, algorithm_choice, alg_params, num_nodes_per_layer, cifar_stat, patch_mode, image_type,)
+#, , , , cifar_stat, patch_mode='Adjacent', image_type='Color'
+DESTIN.setmode(network_mode)
+DESTIN.set_lowest_layer(0)
+# Load Data
+# Modify the location of the training data in file "load_data.py"
+
+# data = np.random.rand(5,32*32*3)
+# Initialize Network; there is is also a layer-wise initialization option
+DESTIN.init_network()
+
+#Train the Network
+print "DeSTIN Training/with out Feature extraction"
+for epoch in range(5):
+    for I in range(data.shape[0]):  # For Every image in the data set
+        if I % 10000 == 0:
+            print("Training Iteration Number %d" % I)
+        for L in range(DESTIN.number_of_layers):
+            if L == 0:
+                img = data[I][:].reshape(32, 32, 3)
+                # This is equivalent to sharing centroids or kernels
+                DESTIN.layers[0][L].load_input(img, [4, 4])
+                DESTIN.layers[0][L].do_layer_learning()
+                #DESTIN.layers[0][L].shared_learning()
+
+            else:
+                DESTIN.layers[0][L].load_input(
+                    DESTIN.layers[0][L - 1].nodes, [2, 2])
+                DESTIN.layers[0][L].do_layer_learning()
+                #DESTIN.layers[0][L].shared_learning()
+    print "Epoch = " + str(epoch+1)
+pickle.dump( DESTIN, open( "DESTIN_conv_[1000, 75, 50 ,25]", "wb" ) )
+print "done"
+
+DESTIN=pickle.load( open( "DESTIN_conv_[1000, 75, 50 ,25]", "rb" ) )
+
+
+
+print("DesTIN running/Feature Extraction/ over the Training Data")
+network_mode = False
+DESTIN.setmode(network_mode)
+
+# Testin it over the training set
+[data, labels] = loadCifar(10)
+del labels
+
+if not os.path.exists('train'):
+    os.makedirs('train')
+
+for I in range(data.shape[0]):  # For Every image in the data set
+    if I % 1000 == 0:
+        print("Testing Iteration Number %d" % I)
+    for L in range(DESTIN.number_of_layers):
+        if L == 0:
+            img = data[I][:].reshape(32, 32, 3)
+            DESTIN.layers[0][L].load_input(img, [4, 4])
+            DESTIN.layers[0][L].do_layer_learning()
+        else:
+            DESTIN.layers[0][L].load_input(
+                DESTIN.layers[0][L - 1].nodes, [2, 2])
+            DESTIN.layers[0][L].do_layer_learning()
+    DESTIN.update_belief_exporter(pool_size, True ,'average_exc_pad')             #( maxpool_shape , ignore_border, mode)
+    if I in range(199, 50999, 200):
+        Name = 'train/' + str(I + 1) + '.txt'
+        #file_id = open(Name, 'w')
+        np.savetxt(Name, np.array(DESTIN.network_belief['belief']))
+        #file_id.close()
+        # Get rid-off accumulated training beliefs
+        DESTIN.clean_belief_exporter()
+
+print("Feature Extraction with the test set")
+[data, labels] = loadCifar(6)
+del labels
+
+if not os.path.exists('test'):
+    os.makedirs('test') 
+    
+for I in range(data.shape[0]):  # For Every image in the data set
+    if I % 1000 == 0:
+        print("Testing Iteration Number %d" % (I+50000))
+    for L in range(DESTIN.number_of_layers):
+        if L == 0:
+            img = data[I][:].reshape(32, 32, 3)
+            DESTIN.layers[0][L].load_input(img, [4, 4])
+            DESTIN.layers[0][L].do_layer_learning()  # Calculates belief for
+        else:
+            DESTIN.layers[0][L].load_input(
+                DESTIN.layers[0][L - 1].nodes, [2, 2])
+            DESTIN.layers[0][L].do_layer_learning()
+    DESTIN.update_belief_exporter(pool_size, True ,'average_exc_pad') 
+    if I in range(199, 10199, 200):
+        Name = 'test/' + str(I + 1) + '.txt'
+        np.savetxt(Name, np.array(DESTIN.network_belief['belief']))
+        # Get rid-off accumulated training beliefs
+        DESTIN.clean_belief_exporter()
+
+
+
+print "Training With SVM"
+print("Loading training and test labels")
+[trainData, trainLabel] = loadCifar(10)
+del trainData
+[testData, testLabel] = loadCifar(6)
+del testData
+
+# Load Training and Test Data/Extracted from DeSTIN
+
+# here we do not use the whole set of feature extracted from DeSTIN
+# We use the features which are extracted from the top few layers
+print("Loading training and testing features")
+
+I = 199
+Name = 'train/' + str(I + 1) + '.txt'
+trainData = np.ravel(np.loadtxt(Name))
+
+
+for I in range(399, 50000, 200):
+    Name = 'train/' + str(I + 1) + '.txt'
+    file_id = open(Name, 'r')
+    Temp = np.ravel(np.loadtxt(Name))
+    trainData = np.hstack((trainData, Temp))
+
+del Temp
+
+
+
+Len = np.shape(trainData)[0]
+Size = np.size(trainData)
+
+Width = Len/50000
+print Len
+print Width*50000
+trainData = trainData.reshape((50000, Width))
+
+# Training SVM
+SVM = svm.LinearSVC(C=1)
+# C=100, kernel='rbf')
+print "Training the SVM"
+trainLabel = np.squeeze(np.asarray(trainLabel).reshape(50000, 1))
+#print trainData
+SVM.fit(trainData, trainLabel)
+print("Training Score = %f " % float(100 * SVM.score(trainData, trainLabel)))
+#print("Training Accuracy = %f" % (SVM.score(trainData, trainLabel) * 100))
+eff = {}
+eff['train'] = SVM.score(trainData, trainLabel) * 100
+del trainData
+
+
+testData = np.array([])
+print("Loading training and testing features")
+
+I = 399
+Name = 'test/' + str(I + 1) + '.txt'
+testData = np.ravel(np.loadtxt(Name))
+
+for I in range(599, 10000, 200):
+    Name = 'test/' + str(I + 1) + '.txt'
+    file_id = open(Name, 'r')
+    Temp = np.ravel(np.loadtxt(Name))
+    testData = np.hstack((testData, Temp))
+
+del Temp
+
+Len = np.shape(testData)[0]
+Size = np.size(testData)
+
+
+I = 399
+Name = 'test/' + str(I + 1) + '.txt'
+testData1 = np.ravel(np.loadtxt(Name))
+print np.shape(testData1)[0]/200.0
+
+
+
+
+Width = np.float(Len)/9800.0
+print Len
+print Size
+testData = testData.reshape((9800, Width))
+
+print "Predicting Test samples"
+print("Test Score = %f" % float(100 * SVM.score(testData, testLabel[200:10000])))
+#print("Training Accuracy = %f" % (SVM.score(testData, testLabel) * 100))
+eff['test'] = SVM.score(testData, testLabel[200:10000]) * 100
+io.savemat('accuracy.mat', eff)
+
+
+
+
+
+
+
